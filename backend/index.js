c0a78ef35b1ea16caa8f384c46b634f72b0deaaf
vaishali sahu <vaishalisahu682@gmail.com>
2025-09-15 11:23:45 +0530

    backend  commit

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://rentalwebsite-frontend.onrender.com",
     credentials:true
 }))
 
